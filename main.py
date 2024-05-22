0668e721dda461a09676211ec7894593f88ae9c9
natan <natan.carvalho123@hotmail.com>
2024-05-21 21:04:47 -0300

    build: Addin packages customtkinter and CTKMessagebox

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import tkinter as tk
+
 from view.login_view import TelaLogin
 
-# Cria a janela de login
-root = tk.Tk()
+# # Cria a janela de login
+import customtkinter
+
+# Modes: system (default), light, dark
+customtkinter.set_appearance_mode("dark")
+# Themes: blue (default), dark-blue, green
+customtkinter.set_default_color_theme("dark-blue")
+
+root = customtkinter.CTk()
 app = TelaLogin(root)
 
 # Inicia o loop da aplicação
--- view/login_view.py
+++ view/login_view.py
@@ -1,25 +1,24 @@
-import tkinter as tk
+import customtkinter as ctk
+from CTkMessagebox import CTkMessagebox
 from view.main_view import MainView
-from tkinter import messagebox
 
 
 class TelaLogin:
     def __init__(self, root):
         self.root = root
         self.root.title("Login")
-        self.root.geometry("300x150")
+        self.root.geometry("400x250")
 
-        self.label_username = tk.Label(root, text="Usuário:")
-        self.label_username.pack()
-        self.entry_username = tk.Entry(root)
-        self.entry_username.pack()
+        self.text = ctk.CTkLabel(root, text="Fazer Login")
+        self.text.pack_configure(padx=10, pady=10)
+        self.entry_username = ctk.CTkEntry(root, placeholder_text="E-mail")
+        self.entry_username.pack(padx=10, pady=10)
 
-        self.label_password = tk.Label(root, text="Senha:")
-        self.label_password.pack()
-        self.entry_password = tk.Entry(root, show="*")
-        self.entry_password.pack()
+        self.entry_password = ctk.CTkEntry(
+            root, placeholder_text="Senha", show="*")
+        self.entry_password.pack(padx=10, pady=10)
 
-        self.button_login = tk.Button(
+        self.button_login = ctk.CTkButton(
             root, text="Login", command=self.validar_login)
         self.button_login.pack(pady=10)
 
@@ -33,8 +32,9 @@ class TelaLogin:
             self.root.withdraw()
 
             # Cria uma nova janela
-            nova_janela = tk.Toplevel()
+            nova_janela = ctk.CTkToplevel()
             app = MainView(nova_janela)
 
         else:
-            messagebox.showerror("Erro", "Credenciais incorretas")
+            CTkMessagebox(
+                title="Erro", message="Credenciais incorretas", icon="cancel")
